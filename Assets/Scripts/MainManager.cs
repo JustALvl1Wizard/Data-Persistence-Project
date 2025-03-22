4fac7bf4b1063cbe6a74dfefd51e3735e2c8e63d
JustALvl1Wizard <andystevens182@gmail.com>
2025-03-22 11:17:15 -0400

    updated to respawn bricks
    created a real gameplay loop with respawn functionality so the game can persist after all bricks destroyed because it wasn't actually finished. now its a buildable game

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -12,32 +12,19 @@ public class MainManager : MonoBehaviour
 
     public Text ScoreText;
     public GameObject GameOverText;
-    
+
     private bool m_Started = false;
     private int m_Points;
-    
+
     private bool m_GameOver = false;
 
-    
-    // Start is called before the first frame update
+    // Called at the start of the scene
     void Start()
     {
-        const float step = 0.6f;
-        int perLine = Mathf.FloorToInt(4.0f / step);
-        
-        int[] pointCountArray = new [] {1,1,2,2,5,5};
-        for (int i = 0; i < LineCount; ++i)
-        {
-            for (int x = 0; x < perLine; ++x)
-            {
-                Vector3 position = new Vector3(-1.5f + step * x, 2.5f + i * 0.3f, 0);
-                var brick = Instantiate(BrickPrefab, position, Quaternion.identity);
-                brick.PointValue = pointCountArray[i];
-                brick.onDestroyed.AddListener(AddPoint);
-            }
-        }
+        SpawnBricks();
     }
 
+    // Update is called once per frame
     private void Update()
     {
         if (!m_Started)
@@ -60,14 +47,43 @@ public class MainManager : MonoBehaviour
                 SceneManager.LoadScene(2);
             }
         }
+        else 
+        {
+            // Check if all bricks have been destroyed
+            if (Object.FindObjectsByType<Brick>(FindObjectsSortMode.None).Length == 0)
+            {
+                SpawnBricks();
+            }
+        }
     }
 
+    // Method that handles instantiating bricks
+    void SpawnBricks()
+    {
+        const float step = 0.6f;
+        int perLine = Mathf.FloorToInt(4.0f / step);
+
+        int[] pointCountArray = new[] { 1, 1, 2, 2, 5, 5 };
+        for (int i = 0; i < LineCount; ++i)
+        {
+            for (int x = 0; x < perLine; ++x)
+            {
+                Vector3 position = new Vector3(-1.5f + step * x, 2.5f + i * 0.3f, 0);
+                var brick = Instantiate(BrickPrefab, position, Quaternion.identity);
+                brick.PointValue = pointCountArray[i];
+                brick.onDestroyed.AddListener(AddPoint);
+            }
+        }
+    }
+
+    // Adds points when a brick is destroyed
     void AddPoint(int point)
     {
         m_Points += point;
         ScoreText.text = $"Score : {m_Points}";
     }
 
+    // Called when the game is over
     public void GameOver()
     {
         m_GameOver = true;
